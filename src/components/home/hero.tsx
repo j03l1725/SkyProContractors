69e1d4e379e129220c432a2e103d022317e6c975
j03l1725 <joelusa25@gmail.com>
2026-01-05 23:01:18 -0500

    Errores de tuplas

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -30,7 +30,7 @@ export function Hero() {
             y: 0,
             transition: {
                 duration: 0.8,
-                ease: [0.6, 0.01, 0.05, 0.95],
+                ease: [0.6, 0.01, 0.05, 0.95] as const,
             },
         },
     };
